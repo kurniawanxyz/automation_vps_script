f103dfa1c5d8996a55a5639a662dd9a0042a471d
Verdi <104150927+gioVerdiansyah@users.noreply.github.com>
2024-09-15 23:06:20 +0700

    fix: host

--- example-apps/python/app.py
+++ example-apps/python/app.py
@@ -7,4 +7,4 @@ def hello_world():
     return "<p>Hello, Python!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True,host="localhost", port=4545)
+    app.run(debug=True,host="0.0.0.0", port=4545)
